3f15a031a6c37ed81f9b24f0ee1708cce22867f6
Gareth Rylance <gareth.rylance@auroraer.com>
2021-02-11 10:32:44 +0000

    Version Bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="aurora-amun-sdk",
-    version="0.1.8",
+    version="0.1.9",
     description="",
     url="https://github.com/AuroraEnergyResearch/aurora-amun-python-sdk",
     author="Aurora Development team",
