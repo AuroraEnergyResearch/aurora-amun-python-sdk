a78490c6682c0c761483df8e5bb2c4bfd6f9ae83
Gareth Rylance <gareth.rylance@auroraer.com>
2021-08-25 10:37:37 +0100

    Update p50scaling value

--- examples/get_load_factors.py
+++ examples/get_load_factors.py
@@ -114,7 +114,7 @@ def main():
     )
     run_request_and_save(
         session,
-        P50YieldScalingParameters(annualProductionInGwHours=2000),
+        P50YieldScalingParameters(annualProductionInGwHours=200),
         base_parameters,
     )
     speeds = get_json("examples\data\example_windSpeed.json")["speeds"]
