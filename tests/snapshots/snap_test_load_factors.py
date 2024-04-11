cfe8b6921f0dc28bae59a857c830a0b336ed9c06
Shivakumar <163876375+shiva-aurora@users.noreply.github.com>
2024-04-11 16:09:43 +0100

    reset snapshot update

--- tests/snapshots/snap_test_load_factors.py
+++ tests/snapshots/snap_test_load_factors.py
@@ -28,7 +28,7 @@ snapshots['test_v1_and_v2_calculations result_v1'] = {
         'regionCode': 'aus_qld',
         'smoothingCoefficient': None,
         'startTimeUTC': '2018-01-01T00:00:00.000Z',
-        'turbineModelId': 280,
+        'turbineModelId': 268,
         'usePowerCurveSmoothing': False,
         'windType': 'P50Scaling'
     },
